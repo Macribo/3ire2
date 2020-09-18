93116c808dfc17b7cab2ff99859d606c472ad6fa
macribo <roibeard.mac.unfraidh@gmail.com>
2020-09-18 16:00:58 +0200

    Commit, then tofuDb init.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { check, validationResult } = require('express-validator');
 
 mongoose.connect('mongodb://localhost:27017/tofuDb', { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-// var allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://spinner-project.herokuapp.com'];
+// var allowedOrigins = ['http://lolscalhost:8080', 'http://localhost:1234', 'https://spinner-project.herokuapp.com'];
 
 //middleware.
 app.use(cors({
